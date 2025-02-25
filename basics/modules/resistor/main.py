9cc702bd0b018725e0e96e3ef46dc3bfe631dd27
teiniker <egon.teiniker@gmail.com>
2025-02-25 21:51:29 +0100

    Refactoring.

--- basics/modules/resistor/main.py
+++ basics/modules/resistor/main.py
@@ -4,19 +4,6 @@ if __name__ == '__main__':
 
     # Verify implementation
 
-    try:
-        r1 = Resistor(1000, -1)
-        #...
-    except ValueError as ex:
-        print(f"Resistor object can't be created: {ex}")
-    else:
-        print("No exception thrown.")
-
-
-    try:
-        r1 = Resistor(-1000, 2)
-        #...
-    except ValueError as ex:
-        print(f"Resistor object can't be created: {ex}")
-    finally:
-        print("Do some clean-up.")
+    r1 = Resistor(1000, 2)
+    print(r1.value)
+    print(r1.tolerance)
