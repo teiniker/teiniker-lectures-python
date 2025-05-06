52220c54b5008792f4eb2cb6b8b0b418afa8aa3e
teiniker <egon.teiniker@gmail.com>
2025-05-06 16:13:06 +0200

    Refactoring.

--- libraries/filesystem/text-files/text_read.py
+++ libraries/filesystem/text-files/text_read.py
@@ -1,3 +1,10 @@
+import os
+
+# Get the current working directory
+cwd = os.getcwd()
+print(cwd)
+
+# Read the contents of a text file
 with open('data.txt', 'r', encoding="utf-8") as in_file:
     for line in in_file:
         print(f"> {line}", end='')
